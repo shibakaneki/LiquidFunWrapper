185bcdf43bb4b5a05bc680025816ed13fe9dbbd0
shibakaneki <kanekishiba@gmail.com>
2021-05-09 17:11:53 +0200

    - Migrated to AndroidX - Removed dependency on jcenter() - Preparing for publication on mavenCentral ( not ready yet )

--- app/src/main/java/ch/shibastudio/liquidfunwrapper/MainActivity.java
+++ app/src/main/java/ch/shibastudio/liquidfunwrapper/MainActivity.java
@@ -1,7 +1,7 @@
 package ch.shibastudio.liquidfunwrapper;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import ch.shibastudio.liquidwrapper.dynamics.Body;
 import ch.shibastudio.liquidwrapper.dynamics.BodyDef;
